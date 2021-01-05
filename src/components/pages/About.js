926da9103139010167da06a19b171cea4a6f5f25
Nele Taevere <ntaevere@gmail.com>
2021-01-05 15:26:57 +1000

    initial commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function About() {
+  return (
+    <>
+      <h1>About</h1>
+      <p>This is the Todolost app v1. It is part of a React Crash course.</p>
+    </>
+  )
+}
+
+export default About;
